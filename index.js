cf5076a30899d0ef23eb40f7a70d9e258ed3bf9f
LarreaE <eneko.larrea@ikasle.aeg.eus>
2024-09-13 09:34:44 +0200

    Fetch done

--- index.js
+++ index.js
@@ -1,521 +1,17 @@
 
-let data = {
-	"items": {
-		"item": [{
-				"id": "0001",
-				"type": "donut",
-				"name": "Hell Cake",
-				"ppu": 0.55,
-				"batters": {
-					"batter": [{
-							"id": "1001",
-							"type": "Regular"
-						},
-						{
-							"id": "1002",
-							"type": "Chocolate"
-						},
-						{
-							"id": "1003",
-							"type": "Blueberry"
-						},
-						{
-							"id": "1004",
-							"type": "Devil's Food"
-						}
-					]
-				},
-				"topping": [{
-						"id": "5001",
-						"type": "None"
-					},
-					{
-						"id": "5002",
-						"type": "Glazed"
-					},
-					{
-						"id": "5005",
-						"type": "Sugar"
-					},
-					{
-						"id": "5007",
-						"type": "Powdered Sugar"
-					},
-					{
-						"id": "5006",
-						"type": "Chocolate with Sprinkles"
-					},
-					{
-						"id": "5003",
-						"type": "Chocolate"
-					},
-					{
-						"id": "5004",
-						"type": "Maple"
-					}
-				],
-				"nutrition_facts": {
-					"nutrition": {
-						"calories": 345,
-						"fat": {
-							"daily_value": "8%",
-							"fat_type": {
-								"saturated": "5.5g",
-								"trans": "1.3g"
-							}
-						},
-						"cholesterol": {
-							"daily_value": "12%",
-							"amount": "12mg"
-						},
-						"sodium": {
-							"daily_value": "15%",
-							"amount": "280mg"
-						},
-						"carbohydrate": {
-							"daily_value": "6%",
-							"carbs_detail": {
-								"amount": "12 g",
-								"type": {
-									"fibre": "1g",
-									"sugars": "25g"
-								}
-							}
-						},
-						"proteine": "11g",
-						"vitamines": [{
-								"type": "Vitamin A",
-								"percent": "0%"
-							},
-							{
-								"type": "Vitamin C",
-								"percent": "0%"
-							},
-							{
-								"type": "Calcium",
-								"percent": "2%"
-							},
-							{
-								"type": "Iron",
-								"percent": "8%"
-							}
-						]
-					}
-				}
-			},
-			{
-				"id": "0002",
-				"type": "donut",
-				"name": "Funny Wizard",
-				"ppu": 0.54,
-				"batters": {
-					"batter": [{
-							"id": "1001",
-							"type": "Regular"
-						},
-						{
-							"id": "1002",
-							"type": "Chocolate"
-						},
-						{
-							"id": "1003",
-							"type": "Blueberry"
-						},
-						{
-							"id": "1009",
-							"type": "Strawberry"
-						}
-					]
-				},
-				"topping": [{
-						"id": "5001",
-						"type": "None"
-					},
-					{
-						"id": "5002",
-						"type": "Glazed"
-					},
-					{
-						"id": "5012",
-						"type": "Lemon"
-					},
-					{
-						"id": "5005",
-						"type": "Sugar"
-					},
-					{
-						"id": "5007",
-						"type": "Powdered Sugar"
-					},
-					{
-						"id": "5006",
-						"type": "Chocolate with Sprinkles"
-					},
-					{
-						"id": "5003",
-						"type": "Chocolate"
-					},
-					{
-						"id": "5008",
-						"type": "White Chocolate"
-					}
-				],
-				"nutrition_facts": {
-					"nutrition": {
-						"calories": 445,
-						"fat": {
-							"daily_value": "22%",
-							"fat_type": {
-								"saturated": "7.5g",
-								"trans": "1.8g"
-							}
-						},
-						"cholesterol": {
-							"daily_value": "19%",
-							"amount": "22mg"
-						},
-						"sodium": {
-							"daily_value": "15%",
-							"amount": "280mg"
-						},
-						"carbohydrate": {
-							"daily_value": "12%",
-							"carbs_detail": {
-								"amount": "32 g",
-								"type": {
-									"fibre": "10g",
-									"sugars": "45g"
-								}
-							}
-						},
-						"proteine": "13g",
-						"vitamines": [{
-								"type": "Vitamin A",
-								"percent": "0%"
-							},
-							{
-								"type": "Vitamin C",
-								"percent": "0%"
-							},
-							{
-								"type": "Calcium",
-								"percent": "2%"
-							},
-							{
-								"type": "Iron",
-								"percent": "6%"
-							}
-						]
-					}
-				}
-			},
-			{
-				"id": "0003",
-				"type": "donut",
-				"name": "Magic Fusion",
-				"ppu": 0.67,
-				"batters": {
-					"batter": [{
-							"id": "1001",
-							"type": "Regular"
-						},
-						{
-							"id": "1002",
-							"type": "Chocolate"
-						},
-						{
-							"id": "1003",
-							"type": "Blueberry"
-						},
-						{
-							"id": "1004",
-							"type": "Devil's Food"
-						},
-						{
-							"id": "1005",
-							"type": "Mint"
-						},
-						{
-							"id": "1006",
-							"type": "Gluten-free"
-						},
-						{
-							"id": "1009",
-							"type": "Strawberry"
-						}
-					]
-				},
-				"topping": [{
-						"id": "5001",
-						"type": "None"
-					},
-					{
-						"id": "5002",
-						"type": "Glazed"
-					},
-					{
-						"id": "5005",
-						"type": "Sugar"
-					},
-					{
-						"id": "5007",
-						"type": "Powdered Sugar"
-					},
-					{
-						"id": "5006",
-						"type": "Chocolate with Sprinkles"
-					},
-					{
-						"id": "5003",
-						"type": "Chocolate"
-					},
-					{
-						"id": "5004",
-						"type": "Maple"
-					},
-					{
-						"id": "5008",
-						"type": "White Chocolate"
-					}
-				],
-				"nutrition_facts": {
-					"nutrition": {
-						"calories": 395,
-						"fat": {
-							"daily_value": "10%",
-							"fat_type": {
-								"saturated": "9.5g",
-								"trans": "1.8g"
-							}
-						},
-						"cholesterol": {
-							"daily_value": "22%",
-							"amount": "22 mg"
-						},
-						"sodium": {
-							"daily_value": "15%",
-							"amount": "280 mg"
-						},
-						"carbohydrate": {
-							"daily_value": "46%",
-							"carbs_detail": {
-								"amount": "32 g",
-								"type": {
-									"fibre": "1g",
-									"sugars": "55g"
-								}
-							}
-						},
-						"proteine": "11g",
-						"vitamines": [{
-								"type": "Vitamin A",
-								"percent": "0%"
-							},
-							{
-								"type": "Vitamin C",
-								"percent": "0%"
-							},
-							{
-								"type": "Calcium",
-								"percent": "2%"
-							},
-							{
-								"type": "Iron",
-								"percent": "8%"
-							}
-						]
-					}
-				}
-			},
-			{
-				"id": "0004",
-				"type": "donut",
-				"name": "Black Mockaroo",
-				"ppu": 0.58,
-				"batters": {
-					"batter": [{
-							"id": "1001",
-							"type": "Regular"
-						},
-						{
-							"id": "1003",
-							"type": "Blueberry"
-						},
-						{
-							"id": "1004",
-							"type": "Devil's Food"
-						},
-						{
-							"id": "1007",
-							"type": "Peanut"
-						},
-						{
-							"id": "1006",
-							"type": "Gluten-free"
-						}
-					]
-				},
-				"topping": [{
-						"id": "5001",
-						"type": "None"
-					},
-					{
-						"id": "5002",
-						"type": "Glazed"
-					},
-					{
-						"id": "5005",
-						"type": "Sugar"
-					},
-					{
-						"id": "5007",
-						"type": "Powdered Sugar"
-					},
-					{
-						"id": "5006",
-						"type": "Chocolate with Sprinkles"
-					},
-					{
-						"id": "5009",
-						"type": "Extra crunchy"
-					},
-					{
-						"id": "5008",
-						"type": "White Chocolate "
-					}
-				],
-				"nutrition_facts": {
-					"nutrition": {
-						"calories": 485,
-						"fat": {
-							"daily_value": "14%",
-							"fat_type": {
-								"saturated": "7.5g",
-								"trans": "3.3g"
-							}
-						},
-						"cholesterol": {
-							"daily_value": "12%",
-							"amount": "19mg"
-						},
-						"sodium": {
-							"daily_value": "25%",
-							"amount": "299mg"
-						},
-						"carbohydrate": {
-							"daily_value": "9%",
-							"carbs_detail": {
-								"amount": "16g",
-								"type": {
-									"fibre": "13g",
-									"sugars": "28g"
-								}
-							}
-						},
-						"proteine": "11g",
-						"vitamines": [{
-								"type": "Vitamin A",
-								"percent": "5"
-							},
-							{
-								"type": "Vitamin C",
-								"percent": "2%"
-							},
-							{
-								"type": "Calcium",
-								"percent": "2%"
-							},
-							{
-								"type": "Iron",
-								"percent": "8%"
-							}
-						]
-					}
-				}
-			},
-			{
-				"id": "0005",
-				"type": "donut",
-				"name": "Relaxing Alchemy",
-				"ppu": 0.78,
-				"batters": {
-					"batter": [{
-						"id": "1006",
-						"type": "Gluten-free"
-					}]
-				},
-				"topping": [{
-						"id": "5001",
-						"type": "None"
-					},
-					{
-						"id": "5022",
-						"type": "Orange"
-					},
-					{
-						"id": "5025",
-						"type": "Melon"
-					},
-					{
-						"id": "5027",
-						"type": "Banana"
-					},
-					{
-						"id": "5028",
-						"type": "Watermelon"
-					}
-				],
-				"nutrition_facts": {
-					"nutrition": {
-						"calories": 625,
-						"fat": {
-							"daily_value": "38%",
-							"fat_type": {
-								"saturated": "15.5g",
-								"trans": "12.3g"
-							}
-						},
-						"cholesterol": {
-							"daily_value": "78%",
-							"amount": "125 mg"
-						},
-						"sodium": {
-							"daily_value": "35%",
-							"amount": "580 mg"
-						},
-						"carbohydrate": {
-							"daily_value": "56%",
-							"carbs_detail": {
-								"amount": "47g",
-								"type": {
-									"fibre": "1g",
-									"sugars": "64g"
-								}
-							}
-						},
-						"proteine": "31g",
-						"vitamines": [{
-								"type": "Vitamin A",
-								"percent": "0.3%"
-							},
-							{
-								"type": "Vitamin C",
-								"percent": "0.6%"
-							},
-							{
-								"type": "Calcium",
-								"percent": "2%"
-							},
-							{
-								"type": "Iron",
-								"percent": "18%"
-							}
-						]
-					}
-				}
-			}
-		]
-	}
+
+const url = "https://gist.githubusercontent.com/Oskar-Dam/62e7175dc542af53a9d18cb292422425/raw/a6cce2b68ea13a77ec5ea7bdfb4df8f23f9ae95f/donuts.json";
+
+fetch(url)
+  .then(data => {
+	console.log("success");
+	return data.json();
+  }).then(data => {
+ 		
+function separate()
+{
+	console.log("//////////////////////////////////////////////////////////////////////////////////////////////////////////////");
+	
 }
 
 /* 
@@ -531,6 +27,11 @@ donut con menos fibra (+ 50 exp)
 
 */
 
+showDonutsUpdated();
+
+separate();
+
+//this function does not contemplate multiple cases
 function showHighestDonuts()
 {
 	let highestProtein = 0;
@@ -555,7 +56,6 @@ function showHighestDonuts()
 	
 
 		let drawFibre = []; // array por si se dan varios casos
-		console.log(ironNum);
 		
 		if (highestProtein < proteinNum) //hacemos comprobacion con el numero mas grande conseguido y el actual.
 		{					
@@ -567,7 +67,7 @@ function showHighestDonuts()
 			donutNameSugar = data.items.item[key].name
 			highestSugar = sugarNum;	
 		}
-		if (leatFibre > fibreNum) // relaxing alchemy and hell cake have the same amount
+		if (leatFibre > fibreNum) // relaxing alchemy and hell tienen el mismo amount
 		{	
 			donutNamefibre = data.items.item[key].name
 			leatFibre = fibreNum;	
@@ -601,16 +101,16 @@ function showDonutsUpdated()
 
 	for (const key in data.items.item) {
 
-		let proteinNum = parseInt(data.items.item[key].nutrition_facts.nutrition.proteine) || 0; // Convertimos el string a un número o lo establecemos en 0 si no es válido
+		let proteinNum = parseInt(data.items.item[key].nutrition_facts.nutrition.proteine) || 0; // convertimos el string a un numero
 		let sugarNum = parseInt(data.items.item[key].nutrition_facts.nutrition.carbohydrate.carbs_detail.type.sugars) || 0;
 		let fibreNum = parseInt(data.items.item[key].nutrition_facts.nutrition.carbohydrate.carbs_detail.type.fibre) || Number.MAX_SAFE_INTEGER;
 		let ironNum = parseInt(data.items.item[key].nutrition_facts.nutrition.vitamines[3].percent) || 0; 
 		//comparaciones
 		if (proteinNum > highestProtein) {
 			highestProtein = proteinNum;
-			donutNamesProtein = [data.items.item[key].name]; // Reiniciamos la lista con el nuevo más alto
+			donutNamesProtein = [data.items.item[key].name];
 		} else if (proteinNum === highestProtein) {
-			donutNamesProtein.push(data.items.item[key].name); // Agregamos si es un empate
+			donutNamesProtein.push(data.items.item[key].name); // agregamos si es un empate
 		}
 
 		if (sugarNum > highestSugar) {
@@ -657,6 +157,7 @@ function showDonutsUpdated()
 */
 
 listDonuts();
+separate();
 
 function listDonuts()
 {
@@ -741,10 +242,6 @@ function showVitamines()
 	console.log("La media de vitamina A es de: " + mediaVitA + "%");
 	console.log("La media de Calcio es de: " + mediaCal + "%");
 	console.log("La media de hierro es de: " + mediaIron + "%");
-
-	// console.log(data.items.item.length);
-	
-
 	
 }
 
@@ -760,6 +257,7 @@ function showVitamines()
 
 posibleMasses();
 posibleToppings();
+separate();
 
 function posibleMasses()
 {
@@ -790,6 +288,7 @@ function posibleToppings()
 */
 
 donutPurchase();
+separate();
 
 function donutPurchase()
 {
@@ -877,3 +376,9 @@ function modifyDonuts()
 			}
 	}
 }
+ 
+  })
+  .catch(error => {
+    // error
+    console.error("Error: "+ error);
+  });
